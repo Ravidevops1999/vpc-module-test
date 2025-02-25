caacf85c0b83e4c3d10defd40e5239bd6548f7f9
Raviteja <rajaraviteja.marampudi1999@gmail.com>
2025-02-25 20:33:37 +0530

    private subnet

--- vpc.tf
+++ vpc.tf
@@ -3,4 +3,6 @@ module "vpc"{
     project_name = var.project_name
     environment  = var.environment
     common_tags = var.common_tags
+    public_subnet_cidrs = var.public_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
 }
